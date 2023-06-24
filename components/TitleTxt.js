50fd27c817fc645e1b2c006cf678a9e0b859aa45
Nimish Lingesh <111670449+NimishLingesh@users.noreply.github.com>
2023-06-24 16:36:06 -0700

    adding uncommited files

---
+++ components/TitleTxt.js
@@ -0,0 +1,19 @@
+import { Text, StyleSheet } from "react-native";
+
+function TitleTxt({children}) {
+    return <Text style={styles.title}>{children}</Text>
+}
+
+export default TitleTxt;
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        color: 'black',
+        textAlign: 'center',
+        padding: 10,
+        borderWidth: 2,
+        borderColor: 'black',
+        borderRadius: 6
+    },
+});
---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+const Colors = {
+    primary500: '#72063c',
+    btns: 'pink'
+    // primary600: 
+}
+
+export default Colors
